e3975849c9a6485629611032cf9564060dc557f7
Drew Cook <drewcook29@gmail.com>
2021-05-11 16:29:58 -0600

    add about page

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+	<v-row>
+		<v-col class="text-center">
+			<h1>About Me</h1>
+			<img src="/v.png" alt="Vuetify.js" class="mb-5" />
+		</v-col>
+	</v-row>
+</template>
+
+<script>
+	export default {}
+</script>
