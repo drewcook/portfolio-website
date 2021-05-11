3f9ac3ef31c02b47adef547349bb8e0662fa263f
Drew Cook <drewcook29@gmail.com>
2021-05-11 16:30:13 -0600

    simplify homepage

--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,24 @@
 			<v-card>
 				<v-card-title class="headline"> Hello World, I'm Drew Cook! </v-card-title>
 				<v-card-text>
-					<p>Here are some of my projects.</p>
-					<ul>
-						<li><a href="https://beerbuddy.io" target="_blank">BeerBuddy</a></li>
-						<li><a href="https://dadjokes.drewcook.dev" target="_blank">Dad Jokes</a></li>
-						<li><a href="https://flickfinder.drewcook.dev" target="_blank">Flick Finder</a></li>
-						<li><a href="https://guitarxc.drewcook.dev" target="_blank">Guitar Exchange</a></li>
-						<li><a href="https://yougotserved.drewcook.dev" target="_blank">You Got Served</a></li>
-					</ul>
+					<p>
+						Welcome to my online portfolio. Check out some of my work or get in touch with me via my
+						contact information.
+					</p>
 					<hr class="my-3" />
-					<a href="https://github.com/drewcook" target="_blank" rel="noopener noreferrer">
-						GitHub
-					</a>
+					<p>Feel free to connect with me on my other platforms, or check out my code on Github.</p>
+					<ul>
+						<li>
+							<a href="https://linkedin.com/in/drewcook2" target="_blank" rel="noopener noreferrer">
+								LinkedIn
+							</a>
+						</li>
+						<li>
+							<a href="https://github.com/drewcook" target="_blank" rel="noopener noreferrer">
+								GitHub
+							</a>
+						</li>
+					</ul>
 				</v-card-text>
 				<v-card-actions>
 					<v-spacer />
