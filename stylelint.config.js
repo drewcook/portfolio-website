b61111d52eb57de3f927f757ab4167705e96ae4c
Drew Cook <drewcook29@gmail.com>
2021-05-12 14:38:53 -0600

    extend stylelint, bring in mdi svgs

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {},
+	extends: [
+		'stylelint-config-standard',
+		'stylelint-config-prettier',
+		'stylelint-config-sass-guidelines',
+	],
+	// add your custom config here
+	// https://stylelint.io/user-guide/configuration
+	rules: {
+		indentation: 'tab',
+	},
 }
