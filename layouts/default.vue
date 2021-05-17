d1a6898481d678fe54d2a7f72415b6380539ece1
Drew Cook <drewcook29@gmail.com>
2021-05-16 19:03:10 -0600

    footer cta

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,18 +10,53 @@
 			</v-container>
 		</v-main>
 
-		<!-- Site Footer -->
-		<v-footer>
+		<!-- Contact CTA -->
+		<section class="footer-cta">
 			<v-container>
-				<p>
-					<a
-						rel="noopener noreferrer"
-						href="https://github.com/drewcook/portfolio-website"
-						target="_blank"
-						>Designed &amp; Built by Drew Cook.</a
-					>
-				</p>
-				<p><small>&copy; Althea Web Services, LLC. All Rights Reserved.</small></p>
+				<v-row>
+					<v-spacer />
+					<v-col cols="12" sm="8">
+						<v-card class="text-center card" shaped>
+							<h3>Would you like to work together?</h3>
+							<p>Don't hesitate to reach out if you'd like to connect.</p>
+							<v-btn color="accent" nuxt to="/contact">Get In Touch</v-btn>
+						</v-card>
+					</v-col>
+					<v-spacer />
+				</v-row>
+			</v-container>
+		</section>
+
+		<!-- Site Footer -->
+		<v-footer class="footer-bar">
+			<v-container>
+				<v-row>
+					<v-col cols="12" sm="6" class="copyright">
+						<small>
+							&copy;
+							<a
+								rel="noopener noreferrer"
+								href="https://github.com/drewcook/portfolio-website"
+								target="_blank"
+								class="content-link"
+							>
+								Althea Web Services, LLC.
+							</a>
+							All Rights Reserved.
+						</small>
+					</v-col>
+					<v-col cols="12" sm="6" class="builtby">
+						<small>
+							<a
+								rel="noopener noreferrer"
+								href="https://github.com/drewcook/portfolio-website"
+								target="_blank"
+								class="content-link"
+								>Designed &amp; Built by Drew Cook.</a
+							>
+						</small>
+					</v-col>
+				</v-row>
 			</v-container>
 		</v-footer>
 	</v-app>
@@ -40,5 +75,44 @@
 
 	#site-content {
 		margin-top: $header-height;
+	}
+
+	.footer-cta {
+		background-color: $dc-neutral;
+		border-bottom: 10px solid $dc-blue-lt;
+		border-top: 10px solid $dc-blue-lt;
+		padding: 60px 0;
+
+		.card {
+			padding: 30px;
+		}
+	}
+
+	.footer-bar {
+		background-color: $dc-dark !important;
+		color: $dc-neutral !important;
+		font-size: 14px;
+		padding: 10px 0;
+		text-align: center;
+
+		a {
+			color: $dc-neutral;
+		}
+
+		a:hover {
+			color: $dc-blue-lt;
+		}
+	}
+
+	.copyright {
+		@media (min-width: 600px) {
+			text-align: left;
+		}
+	}
+
+	.builtby {
+		@media (min-width: 600px) {
+			text-align: right;
+		}
 	}
 </style>
