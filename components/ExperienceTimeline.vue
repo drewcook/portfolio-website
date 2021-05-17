e12d026b26759b3a7d091078fb830ade7de0efb7
Drew Cook <drewcook29@gmail.com>
2021-05-16 21:09:26 -0600

    add in initial timeline

---
+++ components/ExperienceTimeline.vue
@@ -0,0 +1,107 @@
+<template>
+	<div class="experienece-timeline">
+		<div class="dc-timeline">
+			<ul>
+				<li>
+					<div class="content">
+						<h3>Webmastering</h3>
+						<p>
+							I am introduced to basic HTML, CSS, and JavaScript for building webpages as part of a
+							senior year class in high school. The good ole days.
+						</p>
+					</div>
+					<div class="time">
+						<h4>August 2007</h4>
+					</div>
+				</li>
+				<li>
+					<div class="content">
+						<div class="time">
+							<h4>2008</h4>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="content">
+						<div class="time">
+							<h4>2009</h4>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="content">
+						<div class="time">
+							<h4>2010</h4>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="content">
+						<div class="time">
+							<h4>2011</h4>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="content">
+						<div class="time">
+							<h4>2012</h4>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="content">
+						<div class="time">
+							<h4>2013</h4>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="content">
+						<div class="time">
+							<h4>2014</h4>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="content">
+						<h3>Seedpaths</h3>
+						<p>
+							I attend an 8-week intensive bootcamp from a Denver startup and learn fundamental OOP
+							using .NET MVC 4, SQL, lambdas, console applications, and jQuery. Write less, do more.
+						</p>
+					</div>
+					<div class="time">
+						<h4>Sept 2014</h4>
+					</div>
+				</li>
+				<li>
+					<div class="content">
+						<h3>What is Lorem Ipsum?</h3>
+						<p>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+							has been the industry's standard dummy text ever since the 1500s, when an unknown
+							printer took a galley of type and scrambled it to make a type specimen book. It has
+							survived not only five centuries, but also the leap into electronic typesetting,
+							remaining essentially unchanged.
+						</p>
+					</div>
+					<div class="time">
+						<h4>March 2018</h4>
+					</div>
+				</li>
+				<div class="clear" />
+			</ul>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.experience-timeline {
+		margin: 15px auto;
+	}
+
+	.clear {
+		clear: both;
+	}
+</style>
