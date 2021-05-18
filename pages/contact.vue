26144acae761a40d76e67b508e23445dabd95a9a
Drew Cook <drewcook29@gmail.com>
2021-05-18 15:49:16 -0600

    contact info

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,6 +2,14 @@
 	<v-row>
 		<v-col cols="12">
 			<h1>Contact Me</h1>
+			<p>
+				Feel free to send me an email at
+				<a href="mailto:drew@altheaweb.services">drew@altheaweb.services</a>.
+			</p>
+			<p>Or we can connect online.</p>
+			<div class="social-wrap">
+				<contact-icons />
+			</div>
 		</v-col>
 	</v-row>
 </template>
@@ -15,3 +23,9 @@
 		},
 	}
 </script>
+
+<style lang="scss" scoped>
+	.social-wrap {
+		max-width: 240px;
+	}
+</style>
