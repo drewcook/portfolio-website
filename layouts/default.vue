b2d6c6e7dd007d93ec65da49d70fbd2088422370
Drew Cook <drewcook29@gmail.com>
2021-05-18 21:59:49 -0600

    footer additions

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,9 +6,7 @@
 
 		<!-- Page Content -->
 		<v-main id="site-content">
-			<v-container>
-				<nuxt />
-			</v-container>
+			<nuxt />
 		</v-main>
 
 		<!-- Contact CTA -->
@@ -19,8 +17,9 @@
 					<v-col cols="12" sm="8">
 						<v-card class="text-center card" shaped>
 							<h3>Would you like to work together?</h3>
+							<div class="sm-divider" />
 							<p>Don't hesitate to reach out if you'd like to connect.</p>
-							<v-btn color="accent" nuxt to="/contact">Get In Touch</v-btn>
+							<v-btn nuxt to="/contact">Get In Touch</v-btn>
 						</v-card>
 					</v-col>
 					<v-spacer />
@@ -29,39 +28,81 @@
 		</section>
 
 		<!-- Site Footer -->
-		<v-footer class="footer-bar">
-			<v-container>
-				<v-row>
-					<v-col cols="12" sm="6" class="copyright">
-						<small>
-							&copy;
-							<a
-								rel="noopener noreferrer"
-								href="https://github.com/drewcook/portfolio-website"
-								target="_blank"
-								class="content-link"
-							>
-								Althea Web Services, LLC.
-							</a>
-							All Rights Reserved.
-						</small>
-					</v-col>
-					<v-col cols="12" sm="6" class="builtby">
-						<small>
-							<a
-								rel="noopener noreferrer"
-								href="https://github.com/drewcook/portfolio-website"
-								target="_blank"
-								class="content-link"
-								>Designed &amp; Built by Drew Cook.</a
-							>
-						</small>
-					</v-col>
-				</v-row>
-			</v-container>
+		<v-footer class="site-footer flex-column" padless>
+			<section class="footer-links">
+				<v-container>
+					<v-row>
+						<v-col cols="12" sm="6" class="quick-links">
+							<h5>Quick Links</h5>
+							<ul>
+								<li><nuxt-link class="content-link" to="/about">About</nuxt-link></li>
+								<li><nuxt-link class="content-link" to="/work">Work</nuxt-link></li>
+								<li><nuxt-link class="content-link" to="/resume.pdf">Resume</nuxt-link></li>
+								<li><nuxt-link class="content-link" to="/experience">Experience</nuxt-link></li>
+								<li><nuxt-link class="content-link" to="/contact">Contact</nuxt-link></li>
+							</ul>
+						</v-col>
+						<v-col cols="12" sm="6">
+							<h5>Connect With Me</h5>
+							<p class="lead">
+								<a href="mailto:drew@drewcook.dev" class="content-link">
+									<v-icon class="icon">{{ emailIcon }}</v-icon>
+									drew@drewcook.dev
+								</a>
+							</p>
+							<div class="social-wrap">
+								<contact-icons />
+							</div>
+						</v-col>
+					</v-row>
+				</v-container>
+			</section>
+			<section class="footer-floor">
+				<v-container>
+					<v-row>
+						<v-col cols="12" sm="6" class="copyright">
+							<small>
+								&copy;
+								<a
+									rel="noopener noreferrer"
+									href="https://github.com/drewcook/portfolio-website"
+									target="_blank"
+									class="content-link"
+								>
+									Althea Web Services, LLC
+								</a>
+								• All Rights Reserved.
+							</small>
+						</v-col>
+						<v-col cols="12" sm="6" class="builtby">
+							<small>
+								<a
+									rel="noopener noreferrer"
+									href="https://github.com/drewcook/portfolio-website"
+									target="_blank"
+									class="content-link"
+									>Designed &amp; Built by Drew Cook.</a
+								>
+								• {{ new Date().getFullYear() }}
+							</small>
+						</v-col>
+					</v-row>
+				</v-container>
+			</section>
 		</v-footer>
 	</v-app>
 </template>
+
+<script>
+	import { mdiEmail } from '@mdi/js'
+	export default {
+		data() {
+			return {
+				emailIcon: mdiEmail,
+			}
+		},
+	}
+</script>
 
 <style lang="scss" scoped>
 	$header-height-mobile: 115px;
@@ -97,21 +138,22 @@
 
 	.footer-cta {
 		background-color: $dc-neutral;
-		border-bottom: 10px solid $dc-blue-lt;
-		border-top: 10px solid $dc-blue-lt;
+		border-top: 1px solid $dc-gray-lt;
 		padding: 60px 0;
 
 		.card {
 			padding: 30px;
 		}
+
+		.v-btn {
+			background-color: $dc-blue-dk !important;
+			color: #fff;
+		}
 	}
 
-	.footer-bar {
-		background-color: $dc-dark !important;
+	.site-footer {
 		color: $dc-neutral !important;
 		font-size: 14px;
-		padding: 10px 0;
-		text-align: center;
 
 		a {
 			color: $dc-neutral;
@@ -120,17 +162,65 @@
 		a:hover {
 			color: $dc-blue-lt;
 		}
-	}
 
-	.copyright {
-		@media (min-width: 600px) {
-			text-align: left;
+		section {
+			width: 100%;
 		}
-	}
 
-	.builtby {
-		@media (min-width: 600px) {
-			text-align: right;
+		.footer-links {
+			background-color: $dc-blue-md !important;
+			border-bottom: 15px solid $dc-blue-dk;
+			border-top: 15px solid $dc-blue-lt;
+			padding: 40px 0;
+			text-align: center;
+
+			h5 {
+				color: #fff !important;
+				font-size: 20px;
+				margin-bottom: 15px;
+			}
+
+			ul {
+				list-style: none;
+				margin: 0;
+				padding: 0;
+			}
+
+			.social-wrap {
+				margin: auto;
+				max-width: 220px;
+				padding-top: 15px;
+			}
+
+			.lead {
+				font-size: 18px;
+			}
+
+			a:hover {
+				color: $dc-dark;
+			}
+
+			.icon {
+				color: $dc-neutral;
+			}
+		}
+
+		.footer-floor {
+			background-color: $dc-dark !important;
+			padding: 10px 0;
+			text-align: center;
+
+			.copyright {
+				@media (min-width: 600px) {
+					text-align: left;
+				}
+			}
+
+			.builtby {
+				@media (min-width: 600px) {
+					text-align: right;
+				}
+			}
 		}
 	}
 </style>
