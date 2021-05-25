e9283f63b9be4b57629c10856da6871e01ede3d5
drewcook <drewcook29@gmail.com>
2021-05-25 16:52:55 -0600

    experience items

---
+++ assets/js/experience.js
@@ -0,0 +1,81 @@
+const experience = [
+	{
+		timeframe: 'Aug 2007',
+		title: 'Webmastering',
+		category: 'Education',
+		description:
+			'I am introduced to basic HTML, CSS, and JavaScript for building webpages as part of a senior year class in high school. The good ole days.',
+	},
+	{
+		timeframe: '2008 - 2011',
+		title: 'The Books',
+		category: 'Education',
+		description:
+			'I enroll at LSU to study landscape architecture and communications. I learn Autocad, Illustrator and In Design for digital layouts and 3D modelling. Oh the beer.',
+	},
+	{
+		timeframe: 'Sep 2014',
+		title: 'Seedpaths',
+		category: 'Full Time',
+		description:
+			'I attend an 8-week intensive bootcamp from a Denver startup and learn fundamental OOP using .NET MVC 4, SQL, lambdas, console applications, and jQuery. Write less, do more.',
+	},
+	{
+		timeframe: 'Dec 2014 - Feb 2015',
+		title: 'Access Now',
+		category: 'Full Time',
+		description:
+			'I learn Wordpress and Photoshop to design and build websites for a digital marketing agency in Englewood, CO.',
+	},
+	{
+		timeframe: 'Sep 2015 - Apr 2017',
+		title: "Crawford &amp; O'Brien",
+		category: 'Full Time',
+		description:
+			'I lead the technical develepment and maintenance for Wordpress websites for a digital marketing agency within a small team of developers.',
+	},
+	{
+		timeframe: 'Aug 2016',
+		title: 'I start a company.',
+		category: 'Part Time',
+		description:
+			'I register and form Althea Web Services, LLC for the purpose of doing freelance and contract work as an operating business. I also publish any personal products under this name.',
+	},
+	{
+		timeframe: 'Apr 2017 - Aug 2018',
+		title: 'Evolver Inc.',
+		category: 'Full Time',
+		description:
+			'I switch back into .NET development on an enterprise level, and incorporate React and ES6 onto client applications while also providing full-stack support with C#, MongoDB, and SQL.',
+	},
+	{
+		timeframe: 'Apr 2017 - Jul 2019',
+		title: 'Architect of the Capitol',
+		category: 'Contract',
+		description:
+			'I provide full stack .NET and React support on federal projects for both the organization and their clients. I owned all client-side code as we were a small team, rewriting enterprise .NET MVC apps into WebAPIs with React and ES6. No more jQuery.',
+	},
+	{
+		timeframe: 'Jun 2019 - Sep 2020',
+		title: 'Gloo',
+		category: 'Full Time',
+		description:
+			"I work in the frontend capacity within a medium size engineering department for a tech startup, working with Next.js, React, and Apollo's GraphQL for a series of platform products. I also wrote and owned large parts of the accounts, sign on, and onboarding flows that thousands of users experience.",
+	},
+	{
+		timeframe: 'Sep 2020 - May 2021',
+		title: 'Architect of the Capitol',
+		category: 'Contract',
+		description:
+			'I work with a new team to rewrite the existing app I worked on in 2018 to migrate from .NET MVC 4 to .NET Core 3. I also take the opportunity to clean up my code and rewrite all the class-based components into function components with hooks, and migrate from Jest and Enzyme to React Testing Library. I wrote over 300 tests, so many tests.',
+	},
+	{
+		timeframe: 'Present',
+		title: "Now We're Here",
+		category: 'Be In The Now',
+		description:
+			"If you've made it this far, enhorabuenas! You know more about me than I probably do of you. But feel free to reach out to me if you'd like to connect!",
+	},
+]
+
+export default experience
