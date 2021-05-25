e6e1ddacfdf3179e601a9e5f47e68e67c0c33685
drewcook <drewcook29@gmail.com>
2021-05-25 17:57:19 -0600

    fixes

--- components/ContactIcons.vue
+++ components/ContactIcons.vue
@@ -1,31 +1,34 @@
 <template>
-	<div class="d-flex justify-space-around align-center">
-		<a href="https://linkedin.com/in/drewcook2" target="_blank" rel="noopener noreferrer">
-			<v-btn color="primary" elevation="2" fab class="hvr-grow" small>
-				<v-icon color="secondary">{{ linkedInIcon }}</v-icon>
-			</v-btn>
-		</a>
-		<a href="https://github.com/drewcook" target="_blank" rel="noopener noreferrer">
-			<v-btn color="primary" elevation="2" fab class="hvr-grow" small>
-				<v-icon color="secondary">{{ githubIcon }}</v-icon>
-			</v-btn>
-		</a>
-		<a href="https://www.npmjs.com/~drewcook" target="_blank" rel="noopener noreferrer">
-			<v-btn color="primary" elevation="2" fab class="hvr-grow" small>
-				<v-icon large color="secondary">{{ npmIcon }}</v-icon>
-			</v-btn>
-		</a>
-		<a href="https://soundcloud.com/thenotesbetween" target="_blank" rel="noopener noreferrer">
-			<v-btn color="primary" elevation="2" fab class="hvr-grow" small>
-				<v-icon color="secondary">{{ soundcloudIcon }}</v-icon>
-			</v-btn>
-		</a>
+	<div class="contact-links">
+		<div class="d-flex justify-space-around align-center">
+			<a href="https://linkedin.com/in/drewcook2" target="_blank" rel="noopener noreferrer">
+				<v-btn color="primary" elevation="2" fab class="hvr-grow" small>
+					<v-icon color="secondary">{{ linkedInIcon }}</v-icon>
+				</v-btn>
+			</a>
+			<a href="https://github.com/drewcook" target="_blank" rel="noopener noreferrer">
+				<v-btn color="primaryry" elevation="2" fab class="hvr-grow" small>
+					<v-icon color="secondary">{{ githubIcon }}</v-icon>
+				</v-btn>
+			</a>
+			<a href="https://www.npmjs.com/~drewcook" target="_blank" rel="noopener noreferrer">
+				<v-btn color="primaryry" elevation="2" fab class="hvr-grow" small>
+					<v-icon color="secondary" large>{{ npmIcon }}</v-icon>
+				</v-btn>
+			</a>
+			<a href="https://soundcloud.com/thenotesbetween" target="_blank" rel="noopener noreferrer">
+				<v-btn color="primaryry" elevation="2" fab class="hvr-grow" small>
+					<v-icon color="secondary">{{ soundcloudIcon }}</v-icon>
+				</v-btn>
+			</a>
+		</div>
 	</div>
 </template>
 
 <script>
 	import { mdiEmail, mdiGithub, mdiLinkedin, mdiNpm, mdiSoundcloud } from '@mdi/js'
 	export default {
+		props: ['color'],
 		data() {
 			return {
 				githubIcon: mdiGithub,
@@ -41,6 +44,10 @@
 <style lang="scss" scoped>
 	.v-btn {
 		background-color: $dc-blue-dk !important;
+
+		&.info {
+			background-color: $dc-green !important;
+		}
 	}
 
 	.v-icon {
