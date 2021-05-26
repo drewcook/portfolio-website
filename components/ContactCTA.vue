8160c61903fbdd71bed8a5eb9ba77c67a03ce67c
drewcook <drewcook29@gmail.com>
2021-05-26 10:46:42 -0600

    layout component abstractions

---
+++ components/ContactCTA.vue
@@ -0,0 +1,40 @@
+<template>
+	<section v-show="$route.name !== 'contact'" class="footer-cta">
+		<v-container>
+			<v-row>
+				<v-spacer />
+				<v-col cols="12" sm="8">
+					<v-card class="text-center card" shaped>
+						<h3>Would you like to work together?</h3>
+						<div class="sm-divider" />
+						<p>Don't hesitate to reach out if you'd like to connect.</p>
+						<v-btn nuxt to="/contact">Get In Touch</v-btn>
+					</v-card>
+				</v-col>
+				<v-spacer />
+			</v-row>
+		</v-container>
+	</section>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style lang="scss" scoped>
+	.footer-cta {
+		background-color: $dc-heading;
+		border-top: 1px dotted $dc-gray;
+		padding: 60px 0;
+
+		.card {
+			padding: 30px;
+		}
+
+		.v-btn {
+			background-color: $dc-blue-dk !important;
+			color: #fff;
+			font-weight: bold;
+		}
+	}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,7 @@
 <template>
-	<v-app dark>
+	<v-app>
+		<!-- Header -->
 		<Header />
-		<div class="scroll-away-spacer" />
-		<div class="spacer" />
 
 		<!-- Page Content -->
 		<v-main id="site-content" :class="$route.path === '/' ? 'is-home' : ''">
@@ -10,87 +9,10 @@
 		</v-main>
 
 		<!-- Contact CTA -->
-		<section v-show="$route.name !== 'contact'" class="footer-cta">
-			<v-container>
-				<v-row>
-					<v-spacer />
-					<v-col cols="12" sm="8">
-						<v-card class="text-center card" shaped>
-							<h3>Would you like to work together?</h3>
-							<div class="sm-divider" />
-							<p>Don't hesitate to reach out if you'd like to connect.</p>
-							<v-btn nuxt to="/contact">Get In Touch</v-btn>
-						</v-card>
-					</v-col>
-					<v-spacer />
-				</v-row>
-			</v-container>
-		</section>
+		<ContactCTA />
 
 		<!-- Site Footer -->
-		<v-footer class="site-footer flex-column" padless>
-			<section class="footer-links">
-				<v-container>
-					<v-row>
-						<v-col cols="12" sm="6" class="quick-links">
-							<h5>Quick Links</h5>
-							<ul>
-								<li><nuxt-link class="content-link" to="/about">About</nuxt-link></li>
-								<li><nuxt-link class="content-link" to="/work">Work</nuxt-link></li>
-								<li><a class="content-link" href="/resume.pdf" target="_blank">Resume</a></li>
-								<li><nuxt-link class="content-link" to="/experience">Experience</nuxt-link></li>
-								<li><nuxt-link class="content-link" to="/contact">Contact</nuxt-link></li>
-							</ul>
-						</v-col>
-						<v-col cols="12" sm="6">
-							<h5>Connect With Me</h5>
-							<p class="lead">
-								<a href="mailto:drew@drewcook.dev" class="content-link">
-									<v-icon class="icon">{{ emailIcon }}</v-icon>
-									drew@drewcook.dev
-								</a>
-							</p>
-							<div class="social-wrap">
-								<contact-icons />
-							</div>
-						</v-col>
-					</v-row>
-				</v-container>
-			</section>
-			<section class="footer-floor">
-				<v-container>
-					<v-row>
-						<v-col cols="12" sm="6" class="copyright">
-							<small>
-								&copy;
-								<a
-									rel="noopener noreferrer"
-									href="https://github.com/drewcook/portfolio-website"
-									target="_blank"
-									class="content-link"
-								>
-									Althea Web Services, LLC
-								</a>
-								• All Rights Reserved.
-							</small>
-						</v-col>
-						<v-col cols="12" sm="6" class="builtby">
-							<small>
-								<a
-									rel="noopener noreferrer"
-									href="https://github.com/drewcook/portfolio-website"
-									target="_blank"
-									class="content-link"
-								>
-									Designed &amp; Built by Drew Cook.
-								</a>
-								• {{ new Date().getFullYear() }}
-							</small>
-						</v-col>
-					</v-row>
-				</v-container>
-			</section>
-		</v-footer>
+		<Footer />
 	</v-app>
 </template>
 
@@ -106,123 +28,11 @@
 </script>
 
 <style lang="scss" scoped>
-	$header-height-mobile: 115px;
-	$header-height: 185px;
-
-	.scroll-away-spacer {
-		background-image: linear-gradient(0deg, rgba(239, 239, 239, 0), rgba(239, 239, 239, 1) 40%);
-		height: $header-height-mobile;
-		position: fixed;
-		width: 100%;
-		z-index: 50;
-
-		@media (min-width: 700px) {
-			height: $header-height;
-		}
-	}
-
-	.spacer {
-		margin-bottom: $header-height-mobile;
-
-		@media (min-width: 700px) {
-			margin-bottom: $header-height;
-		}
-	}
-
 	#site-content {
 		padding-bottom: 60px !important;
 
 		&.is-home {
 			padding-bottom: 0 !important;
-		}
-	}
-
-	.footer-cta {
-		background-color: $dc-heading;
-		border-top: 1px dotted $dc-gray;
-		padding: 60px 0;
-
-		.card {
-			padding: 30px;
-		}
-
-		.v-btn {
-			background-color: $dc-blue-dk !important;
-			color: #fff;
-			font-weight: bold;
-		}
-	}
-
-	.site-footer {
-		color: $dc-neutral !important;
-		font-size: 14px;
-
-		a {
-			color: $dc-neutral;
-		}
-
-		a:hover {
-			color: $dc-blue-lt;
-		}
-
-		section {
-			width: 100%;
-		}
-
-		.footer-links {
-			background-color: $dc-blue-md !important;
-			border-bottom: 15px solid $dc-blue-dk !important;
-			border-top: 15px solid $dc-blue-lt !important;
-			padding: 40px 0;
-			text-align: center;
-
-			h5 {
-				color: #fff !important;
-				font-size: 20px;
-				margin-bottom: 15px;
-			}
-
-			ul {
-				list-style: none;
-				margin: 0;
-				padding: 0;
-			}
-
-			.social-wrap {
-				margin: auto;
-				max-width: 220px;
-				padding-top: 15px;
-			}
-
-			.lead {
-				font-size: 18px;
-			}
-
-			a:hover {
-				color: $dc-dark;
-			}
-
-			.icon {
-				color: $dc-neutral;
-			}
-		}
-
-		.footer-floor {
-			background-color: $dc-dark !important;
-			padding: 10px 0;
-			text-align: center;
-
-			.copyright {
-				@media (min-width: 600px) {
-					text-align: left;
-				}
-			}
-
-			.builtby {
-				@media (min-width: 600px) {
-					text-align: right;
-				}
-			}
 		}
 	}
 </style>
