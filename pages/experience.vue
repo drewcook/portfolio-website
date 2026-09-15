85e98b69c184d8458f6e54a1d54e80dce4eeb3ae
drewcook <drew@dco.dev>
2026-09-15 17:24:11 -0600

    chore: update resume link

--- pages/experience.vue
+++ pages/experience.vue
@@ -55,7 +55,7 @@
 						title: 'Save To Google Drive',
 						icon: mdiGoogleDrive,
 						download: false,
-						href: 'https://drive.google.com/file/d/1F2I9UYIhrzk258aYPo4-mLQJ-06PVJ1V/view',
+						href: 'https://drive.google.com/file/d/1KXl3DJ0Fr6vbLrqhx47lu-VCbnrf0w6w/view?usp=sharing',
 						newTab: true,
 					},
 				],
